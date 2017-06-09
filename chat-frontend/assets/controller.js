9ab4f87f1e634e4b6c90270ecf99541b9e4adccf
Francisco Jefferson dos Santos <fjefferson@hapvida.com.br>
2017-06-09 11:20:32 -0300

    Alterando o link das salas.json & titulo do projeto

--- chat-frontend/assets/controller.js
+++ chat-frontend/assets/controller.js
@@ -2,7 +2,7 @@ angular.module("chatApp",[])
     .controller("chatController", function($scope,$http){
         $scope.salas=[];
         $http({
-            url: "http://www.jestudio.com.br/chat/assets/salas.json",
+            url: "salas.json",
             method: "GET"
         }).then(function(response) {
             $(response.data).each(function(index, element){
---
+++ readme.rst
@@ -0,0 +1,16 @@
+# (Chat online via Socket)
+
+Tecnologias do chat:
+
+ * NodeJS
+ * Angular 1.6
+ * Bootstrap
+
+
+Chat foi feito para estudo, contendo salas, está feito o deploy no [Heroku](https://www.heroku.com/). Caso queira testar o link é:
+ 
+
+ * [Chat Via Socket](http://www.jestudio.com.br/chat) 
+
+Agradecimentos:
+* Ivonildo Lopes
